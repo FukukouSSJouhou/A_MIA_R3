8693744f271c4e34298aa5de3392e4845c21ccd5
kahenteikou <kokkiemouse@gmail.com>
2022-03-08 13:27:31 +0900

    added split

--- A_MIA_R3_Core/faceproc/Facemod.py
+++ A_MIA_R3_Core/faceproc/Facemod.py
@@ -7,10 +7,21 @@ class Facemod:
         self.frames=frames
         self.splitframe=splitkun
     def process(self):
-        capture = cv2.VideoCapture(self.video_path)
+        capture = cv2.VideoCapture(self.filename)
         fps = capture.get(cv2.CAP_PROP_FPS)
-        counterfps=0
+        counterfps=1
         self.facepoint=[]
         # LOAD CASCADE
         cascade_path='./FACE/models/haarcascade_frontalface_default.xml'
         cascade=cv2.CascadeClassifier(cascade_path)
+        self.hantei = 0
+        while counterfps <= self.frames:
+            capture.set(cv2.CAP_PROP_POS_FRAMES, counterfps)
+            ret,frame=capture.read()
+            if(ret == False):
+                counterfps+=self.splitframe
+                continue
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            self.front_face_list = cascade.detectMultiScale(gray)
+            print("{} {}".format(counterfps, self.front_face_list))
+            counterfps+=self.splitframe
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from A_MIA_R3_Core.Face_Process import Face_Process
 
 
 def main():
-    fp=Face_Process("koizumi_7_30.mp4")
+    fp=Face_Process("koizumi_7_30.mp4",30)
     fp.get_videoinfo()
+    fp.process()
     print("Hello")
 
 
