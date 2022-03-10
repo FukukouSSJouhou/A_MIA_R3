ed05acdaa8055f964650eb1d7c26c030fe9bd670
kahenteikou <kokkiemouse@gmail.com>
2022-03-10 22:00:48 +0900

    added

--- A_MIA_R3_Core/Loggingkun/Loggerkun.py
+++ A_MIA_R3_Core/Loggingkun/Loggerkun.py
@@ -3,4 +3,4 @@ from A_MIA_R3_Core.Loggingkun.MIALogger_base import MIALogger_base
 
 class MIALogger(MIALogger_base):
     def __init__(self,baseoutkun,nocrkun,enabledebug:bool = True):
-        super.__init__(baseoutkun,nocrkun,enabledebug)
+        super().__init__(baseoutkun,nocrkun,enabledebug)
