4b51c0c78c22cba5e3674a5d921e547fb90dc6f5
kahenteikou <kokkiemouse@gmail.com>
2022-03-10 22:07:14 +0900

    added

--- A_MIA_R3_Core/Loggingkun/Loggerkun.py
+++ A_MIA_R3_Core/Loggingkun/Loggerkun.py
@@ -2,5 +2,12 @@ from A_MIA_R3_Core.Loggingkun.MIALogger_base import MIALogger_base
 
 
 class MIALogger(MIALogger_base):
+
+    def __timestampbaseout(self,colorcode,strtxt):
+        self.origcolorout(colorcode,strtxt)
+    def __timestampbaseoutnocr(self,strtxt):
+        self.orignocrkunout(strtxt)
     def __init__(self,baseoutkun,nocrkun,enabledebug:bool = True):
-        super().__init__(baseoutkun,nocrkun,enabledebug)
+        self.origcolorout=baseoutkun
+        self.orignocrkunout=nocrkun
+        super().__init__(self.__timestampbaseout,self.__timestampbaseoutnocr,enabledebug)
