52a869a03303b0090a5e04e83edbbef1ac7d1bc5
kahenteikou <kokkiemouse@gmail.com>
2022-03-10 17:25:03 +0900

    added

--- A_MIA_R3_Core/Face_Process.py
+++ A_MIA_R3_Core/Face_Process.py
@@ -5,18 +5,33 @@ from A_MIA_R3_Core.faceproc.Facemod import Facemod
 
 
 class Face_Process:
+    def __init__(self, filename: str, split_frames: int, loggerobj: MIALogger):
+        """
+        コンストラクタだよ～
+        :param filename: ビデオファイル名
+        :param split_frames: 一回当たりのフレーム数
+        :param loggerobj: Logger object
+        """
+        self.filename = filename
+        self.Loggingobj = loggerobj
+        self.total_frames = 0
+        self.split_frames = split_frames
 
-    def __init__(self,filename:str,split_frames:int,loggerobj:MIALogger):
-        self.filename=filename
-        self.Loggingobj=loggerobj
-        self.total_frames=0
-        self.split_frames=split_frames
     def get_videoinfo(self):
-        container=av.open(self.filename)
+        """
+        ビデオ情報取得
+        """
+        container = av.open(self.filename)
         self.total_frames = container.streams.video[0].frames
+
     def process(self):
-        fm=Facemod(self.filename,self.total_frames,self.split_frames,self.Loggingobj)
+        """
+        実際に感情分析の処理を呼び出す。
+
+        :return: 一定フレーム当たりの感情データ
+        """
+        fm = Facemod(self.filename, self.total_frames, self.split_frames, self.Loggingobj)
         fm.target_img_select()
-        #fm.showtargetimage()
+        # fm.showtargetimage()
         fm.process()
         return fm.get_timeemos()
