2989e0f57380e27f0c3d1c4ab85b7021c79f02fa
kahenteikou <kokkiemouse@gmail.com>
2022-03-10 17:42:56 +0900

    added

--- A_MIA_R3_Core/jsonencoder/Numkunencoder.py
+++ A_MIA_R3_Core/jsonencoder/Numkunencoder.py
@@ -4,6 +4,9 @@ import numpy
 
 
 class Numkunencoder(json.JSONEncoder):
+    """
+    JSONにエクスポートするときに使うエンコーダー
+    """
     def default(self, obj):
         if isinstance(obj,numpy.floating):
             return float(obj)
