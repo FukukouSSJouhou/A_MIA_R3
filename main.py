4ec72d33818fd456f30e476c6648478040b90ed1
kahenteikou <kokkiemouse@gmail.com>
2022-03-14 17:38:56 +0900

    added

--- main.py
+++ main.py
@@ -2,13 +2,18 @@
 import os
 import sys
 
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtQml
 
 
 def main():
+    #環境変数をセット
     os.environ["QT_QUICK_CONTROLS_STYLE"] = "Material"
+    #Qtのアプリケーションオブジェクトを生成
     app = QtWidgets.QApplication(sys.argv)
+    engine=QtQml.QQmlApplicationEngine()
+    engine.load("A_MIA_R3.qml")
     
+
 
 if __name__ == '__main__':
     main()
