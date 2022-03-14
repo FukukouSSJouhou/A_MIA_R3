be4e97206ea88893715c6dbcb901fbf8cea83b3b
kahenteikou <kokkiemouse@gmail.com>
2022-03-14 17:30:16 +0900

    added

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import os
+import sys
+
+from PySide2 import QtWidgets
+
+
+def main():
+    os.environ["QT_QUICK_CONTROLS_STYLE"] = "Material"
+    app = QtWidgets.QApplication(sys.argv)
+    
+
+if __name__ == '__main__':
+    main()
