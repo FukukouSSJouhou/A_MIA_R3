7bfd3a45dd6aee0065336c7a62aacec39a492510
kahenteikou <kokkiemouse@gmail.com>
2022-03-20 23:00:58 +0900

    added cb

--- A_MIA_R3_Core/faceproc/FPCallbackFaceSelector.py
+++ A_MIA_R3_Core/faceproc/FPCallbackFaceSelector.py
@@ -16,3 +16,15 @@ class FPCallbackFaceSelector:
         :param target_image: target image
         """
         self.callback(frame, front_face_list,target_image)
+class FPCallbackFaceSelected:
+    def __init__(self,callback):
+        """
+        constructor
+
+        :param callback: callback obj
+        """
+        self.callback=callback
+    def execute(self,target_img):
+        self.callback(target_img)
+
+
