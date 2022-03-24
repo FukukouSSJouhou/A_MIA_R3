218988de43cf975803659d19f7f826f65c6e3c2e
kahenteikou <kokkiemouse@gmail.com>
2022-03-24 09:31:33 +0900

    sex

---
+++ src_node/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+class App extends React.Component{
+    public render():React.ReactNode{
+        return(
+            <>
+            <BrowserRouter>
+                <Routes>
+                    tdn
+                </Routes>
+            </BrowserRouter>
+            </>
+        );
+    }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+module.exports = {
+    mode: "development",
+    entry: "./src_node/index.tsx",
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: "index.js"
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.ts', '.tsx']
+    },
+    devtool: 'source-map',
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+        static: { directory: path.join(__dirname, 'public'), }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src_node/index.html',
+        }),
+        new HtmlWebpackPlugin({
+            template: './src_node/index.html',
+            filename: '404.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src_node/index.html',
+            filename: 'apps.html'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src_node/index.html',
+            filename: 'plamar.html'
+        }),
+        new CopyWebpackPlugin(
+            {
+                patterns:[
+                    {from:"public",to:"./"}
+                ]
+            }
+        )
+    ],
+}
