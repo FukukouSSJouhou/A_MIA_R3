b73fc0e3b3112fb4809a9bc8c5adfeebafff998e
kahenteikou <kokkiemouse@gmail.com>
2022-03-24 09:35:50 +0900

    journal

--- src_node/App.tsx
+++ src_node/App.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import IndexPage from './pages/IndexPage';
 class App extends React.Component{
     public render():React.ReactNode{
         return(
             <>
             <BrowserRouter>
                 <Routes>
-                    tdn
+                    <Route path="/" element={<IndexPage />}/>
+                    <Route path="/index.html" element={<IndexPage />}/>
                 </Routes>
             </BrowserRouter>
             </>
---
+++ src_node/pages/IndexPage.tsx
@@ -0,0 +1,8 @@
+import React, * as react from 'react';
+export default function IndexPage():React.ReactElement{
+    return(
+        <>
+        <h1>Hello World</h1>
+        </>
+    );
+}
