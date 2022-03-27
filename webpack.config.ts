2ea8d98dd95cd6c92d0dba101c249ee617ece7db
kahenteikou <kokkiemouse@gmail.com>
2022-03-27 12:41:01 +0900

    w

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
     mode: "development",
     entry: "./src_node/index.tsx",
