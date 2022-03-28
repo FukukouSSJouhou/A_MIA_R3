2399d49d9d4e8aee44ee1b521602ac35a4cdee1f
kahenteikou <kokkiemouse@gmail.com>
2022-03-28 10:11:07 +0900

    parted

--- src_node/pages/IndexPage.tsx
+++ src_node/pages/IndexPage.tsx
@@ -2,48 +2,50 @@ import { Box, Button, Step, StepLabel, Stepper, Typography } from '@mui/material
 import React, * as react from 'react';
 
 const steps = ['Select File Name', 'Create an ad group', 'Create an ad'];
-export default function IndexPage():React.ReactElement{
-    const [vfilename,setvfilename]=React.useState<string>(null!);
-    const [activeStep, setActiveStep] = React.useState(0);
-    const [skipped, setSkipped] = React.useState(new Set<number>());
-  
-    const isStepOptional = (step: number) => {
-      return step === 1;
-    };
-  
-    const isStepSkipped = (step: number) => {
-      return skipped.has(step);
-    };
-  
-    const button_clicked=()=>{
-        //console.log("clicked");
-        window.mia_electron_api.openVideoFileDialog("Open File").then(
-            (pathobj)=>{
-                if(pathobj.status===true){
-                    setvfilename(pathobj.path);
-                }else{
-                    setvfilename("none");
-                }
-            });
-    };
-    const handleNext = () => {
-        if(activeStep===0){
+export default function IndexPage(): React.ReactElement {
+  const [vfilename, setvfilename] = React.useState<string>(null!);
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set<number>());
 
+  const isStepOptional = (step: number) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step: number) => {
+    return skipped.has(step);
+  };
+
+  const button_clicked = () => {
+    //console.log("clicked");
+    window.mia_electron_api.openVideoFileDialog("Open File").then(
+      (pathobj) => {
+        if (pathobj.status === true) {
+          setvfilename(pathobj.path);
+        } else {
+          setvfilename("none");
         }
-        console.log(activeStep);
-      let newSkipped = skipped;
-      if (isStepSkipped(activeStep)) {
-        newSkipped = new Set(newSkipped.values());
-        newSkipped.delete(activeStep);
-      }
-  
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
-      setSkipped(newSkipped);
-    };
-  
-    const handleBack = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
+      });
+  };
+  const handleNext = () => {
+    switch (activeStep){
+      case 0:
+        console.log("Zero");
+        return;
+    }
+    console.log(activeStep);
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
   /*
     const handleSkip = () => {
       if (!isStepOptional(activeStep)) {
@@ -60,88 +62,88 @@ export default function IndexPage():React.ReactElement{
       });
     };
   */
-    const handleReset = () => {
-      setActiveStep(0);
-    };
-    const naibuyouso=()=>{
-        switch(activeStep){
-            case 0:
-                return (
-                  <>
-                  
-        {vfilename}
-        <br />
-        <Button onClick={button_clicked}>
-            Click me!
-        </Button>
-        <br />
-                  </>
-                );
-            case 1:
-                return "One!";
-            default:
-                return "Default!";
-        }
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+  const naibuyouso = () => {
+    switch (activeStep) {
+      case 0:
+        return (
+          <>
+
+            {vfilename}
+            <br />
+            <Button onClick={button_clicked}>
+              Click me!
+            </Button>
+            <br />
+          </>
+        );
+      case 1:
+        return "One!";
+      default:
+        return "Default!";
     }
-    return(
-        <>
-        <h1>Hello Work!</h1>
-        <br/>
-    <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps: { completed?: boolean } = {};
-          const labelProps: {
-            optional?: React.ReactNode;
-          } = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = (
-              <Typography variant="caption">Optional</Typography>
-            );
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-            {
-                naibuyouso()
+  }
+  return (
+    <>
+      <h1>Hello Work!</h1>
+      <br />
+      <Box sx={{ width: '100%' }}>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps: { completed?: boolean } = {};
+            const labelProps: {
+              optional?: React.ReactNode;
+            } = {};
+            if (isStepOptional(index)) {
+              labelProps.optional = (
+                <Typography variant="caption">Optional</Typography>
+              );
             }
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />   
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
-    </Box>
-    <br />
-        </>
-    )
+            if (isStepSkipped(index)) {
+              stepProps.completed = false;
+            }
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            {
+              naibuyouso()
+            }
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              <Box sx={{ flex: '1 1 auto' }} />
+              <Button onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </Box>
+          </React.Fragment>
+        )}
+      </Box>
+      <br />
+    </>
+  )
 }
