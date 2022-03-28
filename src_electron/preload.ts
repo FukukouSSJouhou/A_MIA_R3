05bc9cf1e728a1dd90df0d3452349b4f8d3d9964
kahenteikou <kokkiemouse@gmail.com>
2022-03-28 10:12:40 +0900

    pro

--- src_electron/preload.ts
+++ src_electron/preload.ts
@@ -4,6 +4,8 @@ contextBridge.exposeInMainWorld(
     {
         sendsample:(data:string)=>ipcRenderer.invoke("sendsample",data),
         onSample:(callback:any)=>ipcRenderer.on("onSample",(event,argv)=>callback(event,argv)),
-        openVideoFileDialog:async(title:string)=>await ipcRenderer.invoke("openVideoFileDialog",title)
+        openVideoFileDialog:async(title:string)=>await ipcRenderer.invoke("openVideoFileDialog",title),
+        fileExistsAsync:async(filename:string):Promise<boolean>=>
+            await ipcRenderer.invoke("fileExistsAsync",filename),
     }
 )
