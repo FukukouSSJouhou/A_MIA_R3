5081b79e73afbb7e3bf7e61f6aba0d483b6ca1e6
kahenteikou <kokkiemouse@gmail.com>
2022-04-02 10:09:14 +0900

    logger

--- A_MIA_R3_Core/nodewrap/A_MIR_R3_node.py
+++ A_MIA_R3_Core/nodewrap/A_MIR_R3_node.py
@@ -1,5 +1,18 @@
 
 class A_MIR_R3_node2(object):
-    def __init__(self,loggerkun):
-        self.loggerkun=loggerkun
-        self.loggerkun("s")
+
+    def logout_color(self,colorcode, txt):
+        """
+        色付きログ出力を行うコードだよ
+
+        :param colorcode: カラーコード
+        :param txt: 出力内容
+        :return:
+        """
+        r = int(colorcode[1:3], 16)
+        g = int(colorcode[3:5], 16)
+        b = int(colorcode[5:7], 16)
+        self.jslog("\033[38;2;{};{};{}m{}\033[0m".format(r, g, b, txt))
+    def __init__(self,jslog):
+        self.jslog=jslog
+        self.logout_color("#FF00FF","Python Class init..")
