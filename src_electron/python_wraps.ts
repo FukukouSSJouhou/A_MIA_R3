83d3497a7ecf2fb2036a2a0dd19c43dea0675a9c
kahenteikou <kokkiemouse@gmail.com>
2022-04-02 09:57:28 +0900

    met

--- src_electron/python_wraps.ts
+++ src_electron/python_wraps.ts
@@ -2,7 +2,7 @@ import pynode from '@fukukoussjouhou/pynode';
 class A_MIA_R3_PythonWraps{
     pyinstance:any;
     constructor(){
-        this.pyinstance=pynode.import("A_MIA_R3_Core.nodewrap.A_MIR_R3_node")
+        this.pyinstance=pynode.pyimport("A_MIA_R3_Core.nodewrap.A_MIR_R3_node")
     }
 }
 export default A_MIA_R3_PythonWraps;
