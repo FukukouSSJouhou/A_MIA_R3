16dec4e8aa037446024e45d4098162220769eab6
kahenteikou <kokkiemouse@gmail.com>
2022-04-02 09:52:19 +0900

    added

---
+++ A_MIA_R3_Core/nodewrap/A_MIR_R3_node.py
@@ -0,0 +1,3 @@
+class A_MIR_R3_node2(object):
+    def __init__(self,testv):
+        print(testv)
--- src_electron/python_wraps.ts
+++ src_electron/python_wraps.ts
@@ -1,8 +1,8 @@
 import pynode from '@fridgerator/pynode';
-
 class A_MIA_R3_PythonWraps{
     pyinstance:any;
     constructor(){
+        this.pyinstance=pynode.import("A_MIA_R3_Core.nodewrap.A_MIR_R3_node")
     }
 }
 export default A_MIA_R3_PythonWraps;
