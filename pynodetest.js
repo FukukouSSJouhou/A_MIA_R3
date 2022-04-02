1f1efac416d0a792b5c8e120ad139269bce06294
kahenteikou <kokkiemouse@gmail.com>
2022-04-02 09:53:47 +0900

    fx

--- pynodetest.js
+++ pynodetest.js
@@ -1,11 +1,11 @@
-const pynode = require('@fridgerator/pynode')
+const pynode = require('@fukukoussjouhou/pynode')
 
 
 // add current path as Python module search path, so it finds our test.py
 pynode.startInterpreter()
 pynode.appendSysPath('./')
 pynode.appendSysPath('./venv/Lib/site-packages')
-const pyclasstest1 = pynode.import('pynodetestmod');
+const pyclasstest1 = pynode.pyimport('pynodetestmod');
 classi=pyclasstest1.get("pyclasstest").call('4','3');
 result=classi.get("run").call((a,b,c)=>{
   console.log(arguments);
