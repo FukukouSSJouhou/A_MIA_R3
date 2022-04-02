2dfcf9b40ae3295f9705f3c79a5d522bdd4f2b4f
kahenteikou <kokkiemouse@gmail.com>
2022-04-02 10:19:49 +0900

    added

--- A_MIA_R3_Core/nodewrap/A_MIR_R3_node.py
+++ A_MIA_R3_Core/nodewrap/A_MIR_R3_node.py
@@ -1,3 +1,5 @@
+from A_MIA_R3_Core.Loggingkun.Loggerkun import MIALogger
+
 
 class A_MIR_R3_node2(object):
 
@@ -16,3 +18,13 @@ class A_MIR_R3_node2(object):
     def __init__(self,jslog):
         self.jslog=jslog
         self.logout_color("#FF00FF","Python Class init..")
+        # Create logger Object
+        self.Loggingobj = MIALogger(self.logout_color, self.jslog)
+        self.filenamekun=""
+    def setFilename(self,filename):
+        self.filenamekun=filename
+        return filename
+    def run(self):
+        self.Loggingobj.successout("Run!!")
+        self.Loggingobj.blueout(self.filenamekun)
+        return 0
--- src_electron/python_wraps.ts
+++ src_electron/python_wraps.ts
@@ -9,5 +9,11 @@ class A_MIA_R3_PythonWraps{
         this.pyclasskun=pynode.pyimport("A_MIA_R3_Core.nodewrap.A_MIR_R3_node")
         this.pyinstance=this.pyclasskun.get("A_MIR_R3_node2").call(this.loggerobj);
     }
+    public async setFilename(filename:string){
+        return this.pyinstance.get("setFilename").call(filename);
+    }
+    public async run():Promise<number>{
+        return this.pyinstance.get("run").call();
+    }
 }
 export default A_MIA_R3_PythonWraps;
