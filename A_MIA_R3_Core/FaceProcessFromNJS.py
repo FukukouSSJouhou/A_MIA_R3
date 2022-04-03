026599f7bfdf78d0291e6a30e883b610fc1d5de4
kahenteikou <kokkiemouse@gmail.com>
2022-04-04 07:58:14 +0900

    added inj

---
+++ A_MIA_R3_Core/FaceProcessFromNJS.py
@@ -0,0 +1,10 @@
+import A_MIA_R3_Core
+from A_MIA_R3_Core.Loggingkun.Loggerkun import MIALogger
+
+
+class FaceProcessFromNJS:
+    def __init__(self,loggingobj:MIALogger,filename:str):
+        self.loggingobj=loggingobj
+        self.filename=filename
+    
+
