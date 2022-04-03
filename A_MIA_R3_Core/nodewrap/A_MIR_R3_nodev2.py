00cdef22058e03c4574ecf53301bfe0a93ac5708
kahenteikou <kokkiemouse@gmail.com>
2022-04-04 07:53:04 +0900

    ??/

---
+++ A_MIA_R3_Core/nodewrap/A_MIR_R3_nodev2.py
@@ -0,0 +1,41 @@
+import base64
+
+import cv2
+
+from A_MIA_R3_Core.Face_Process import Face_Process
+from A_MIA_R3_Core.Loggingkun.Loggerkun import MIALogger
+from A_MIA_R3_Core.faceproc.FPCallbackFaceSelector import FPCallbackFaceSelected, FPCallbackFaceSelector
+
+
+class A_MIR_R3_node2(object):
+
+    def logout_color(self,colorcode, txt):
+        """
+        色付きログ出力を行うコードだよ
+
+        :param colorcode: カラーコード
+        :param txt: 出力内容
+        :return:
+        """
+        r = int(colorcode[1:3], 16)
+        g = int(colorcode[3:5], 16)
+        b = int(colorcode[5:7], 16)
+        self.jslog("\033[38;2;{};{};{}m{}\033[0m".format(r, g, b, txt))
+    def __init__(self,jslog):
+        self.jslog=jslog
+        self.logout_color("#FF00FF","Python Class init..")
+        # Create logger Object
+        self.Loggingobj = MIALogger(self.logout_color, self.jslog)
+        self.filenamekun=""
+        self.imagelistsendcallback=None
+        self.selectimgended=False
+        self.fpselected=None
+        self.load_img_list_origcv = []
+    def setFilename(self,filename):
+        self.filenamekun=filename
+        self.Loggingobj.successout("set Filename:{}".format(filename))
+        return filename
+    def run(self):
+        self.Loggingobj.successout("Run!!")
+        self.Loggingobj.blueout(self.filenamekun)
+        self.Loggingobj.successout("<< A_MIA_R3 Core System>>")
--- src_electron/python_wraps.ts
+++ src_electron/python_wraps.ts
@@ -6,7 +6,7 @@ class A_MIA_R3_PythonWraps{
         console.log(strkun);
     }
     constructor(){
-        this.pyclasskun=pynode.pyimport("A_MIA_R3_Core.nodewrap.A_MIR_R3_node")
+        this.pyclasskun=pynode.pyimport("A_MIA_R3_Core.nodewrap.A_MIR_R3_nodev2")
         this.pyinstance=this.pyclasskun.get("A_MIR_R3_node2").call(this.loggerobj);
     }
     public async setFilename(filename:string):Promise<string>{
@@ -16,10 +16,10 @@ class A_MIA_R3_PythonWraps{
         return this.pyinstance.get("run").call();
     }
     public async Setimagelistsendcallback(callback:(datakun:string)=>void):Promise<void>{
-        this.pyinstance.get("Setimagelistsendcallback").call(callback);
+        //this.pyinstance.get("Setimagelistsendcallback").call(callback);
     }
     public async setselectimg(indexkun:number):Promise<void>{
-        this.pyinstance.get("recieve_selectimg").call(indexkun);
+        //this.pyinstance.get("recieve_selectimg").call(indexkun);
     }
 }
 export default A_MIA_R3_PythonWraps;
