d052f10f473e642e297525e15a89126bf6968d82
kahenteikou <kokkiemouse@gmail.com>
2022-04-04 09:56:53 +0900

    added init

--- A_MIA_R3_Core/nodewrap/A_MIR_R3_node_tensor.py
+++ A_MIA_R3_Core/nodewrap/A_MIR_R3_node_tensor.py
@@ -1,4 +1,17 @@
+import os
+
 from A_MIA_R3_Core.Loggingkun.Loggerkun import MIALogger
+
+
+import numpy as np
+
+import cv2
+
+from keras_preprocessing import image
+from tensorflow.python.keras.models import load_model
+from matplotlib import pyplot as plt
+
+from A_MIA_R3_Core.faceproc.Facemod import path_cutext
 
 
 class A_MIR_R3_node_tensor2(object):
@@ -19,3 +32,15 @@ class A_MIR_R3_node_tensor2(object):
         self.jslog = jslog
         self.logout_color("#FF00FF", "Python Class for Tensorflow init..")
         self.Loggingobj = MIALogger(self.logout_color, self.jslog)
+        model_path = './FACE/models/5face_emotions_100ep.hdf5'
+        self.Loggingobj.normalout("Loading model...")
+        self.emotions_XCEPTION = load_model(model_path, compile=False)
+
+        self.timeemos = []
+        self.targetimage = None
+
+        if not os.path.exists('./FACE/facepointmemo/'):
+            os.makedirs('./FACE/facepointmemo/')
+
+        if not os.path.exists('./FACE/emomemo/'):
+            os.makedirs('./FACE/emomemo/')
