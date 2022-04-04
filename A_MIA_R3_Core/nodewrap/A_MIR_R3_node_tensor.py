cb6bebebf36af4daa70ac665de2b8160884e98b5
kahenteikou <kokkiemouse@gmail.com>
2022-04-04 09:55:23 +0900

    added scripts

---
+++ A_MIA_R3_Core/nodewrap/A_MIR_R3_node_tensor.py
@@ -0,0 +1,21 @@
+from A_MIA_R3_Core.Loggingkun.Loggerkun import MIALogger
+
+
+class A_MIR_R3_node_tensor2(object):
+
+    def logout_color(self,colorcode, txt):
+        """
+        色付きログ出力を行うコードだよ
+
+        :param colorcode: カラーコード
+        :param txt: 出力内容
+        :return:
+        """
+        r = int(colorcode[1:3], 16)
+        g = int(colorcode[3:5], 16)
+        b = int(colorcode[5:7], 16)
+        self.jslog("\033[38;2;{};{};{}m{}\033[0m".format(r, g, b, txt))
+    def __init__(self,jslog):
+        self.jslog = jslog
+        self.logout_color("#FF00FF", "Python Class for Tensorflow init..")
+        self.Loggingobj = MIALogger(self.logout_color, self.jslog)
--- src_electron/tensorpythonchildproc.ts
+++ src_electron/tensorpythonchildproc.ts
@@ -1,5 +1,10 @@
 import TENSORPYTHONPROCINTERFACE from "./tensorpythonproc_interface";
-
+import pynode from '@fukukoussjouhou/pynode';
+const loggerobj=(strkun:string)=>{
+    console.log(strkun);
+}
+let pytensor_class=pynode.pyimport("A_MIA_R3_Core.nodewrap.A_MIR_R3_node_tensor")
+let pyinstance=pytensor_class.get("A_MIR_R3_node_tensor2").call(loggerobj);
 process.on("message",(message:TENSORPYTHONPROCINTERFACE)=>{
     let base64target=message.base64target;
     let filename=message.filename;
