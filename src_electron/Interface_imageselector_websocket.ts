2daca07c5c537534e72a555c39176b10acdd8999
kahenteikou <kokkiemouse@gmail.com>
2022-04-07 17:27:01 +0900

    a

---
+++ src_electron/Interface_imageselector_websocket.ts
@@ -0,0 +1,5 @@
+interface Interface_imageselector_websocket{
+    mode:string;
+    data:any;
+}
+export default Interface_imageselector_websocket;
--- src_electron/imageselector_websocket.ts
+++ src_electron/imageselector_websocket.ts
@@ -1,7 +1,8 @@
 import IimageCVSelector from "./IimageCVSelector";
 import path from "path";
-import {client as WebSocketClient} from "websocket";
+import {client, client as WebSocketClient} from "websocket";
 class imageselector_websocket implements IimageCVSelector{
+    client:WebSocketClient;
     constructor(){
         let pythonpath:string;
         let wspath:string;
@@ -12,7 +13,20 @@ class imageselector_websocket implements IimageCVSelector{
             pythonpath="./venv/Scripts/python";
             wspath="ws://localhost:1919/";
         }
-
+        this.client=new WebSocketClient();
+        this.client.on("connect",(connection)=>{
+            connection.on("error",(err)=>{
+                console.log("Connection Error: " + err.toString());
+            });
+            connection.on("close",()=>{
+                console.log("closed");
+            });
+            connection.on("message",(msg)=>{
+                if(msg.type==="utf8"){
+                    
+                }
+            })
+        })
     }
     public getNextImageBase64(): string {
         throw new Error("Method not implemented.");
